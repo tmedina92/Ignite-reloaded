742948d41aa68d84f43eaabb5060ce555c031fd4
thamis medina <thamismedina@gmail.com>
2022-06-07 18:28:31 -0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Thamis Medina" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam laboriosam eum quibusdam unde cumque? Voluptatibus, nesciunt eligendi. Commodi explicabo magnam officiis incidunt dolores odit dolor neque repellendus, labore repellat voluptatem."
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Lorem ipsum dolor sit amet consectetur"
+      />  
+    </div>
+  )
+}
+
+

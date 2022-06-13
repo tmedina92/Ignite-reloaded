b2b886f1e2f83a0ea7eae3601d106ed3bd531438
thamis medina <thamismedina@gmail.com>
2022-06-13 16:36:01 -0300

    finish style of the app

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://avatars.githubusercontent.com/u/94133870?v=4" alt="" />
+      <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/94133870?v=4" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -15,7 +16,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!! 👏👏</p>

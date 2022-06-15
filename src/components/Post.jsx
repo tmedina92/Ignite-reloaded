b9d32ad2ea643ef7c9e5830191cc7251e9c6cfab
thamis medina <thamismedina@gmail.com>
2022-06-15 14:58:38 -0300

    add likes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,6 +13,8 @@ export function Post({ author, publishedAt, content }) {
   ])
   const [newCommentText, setNewCommentText] = useState('')
 
+  console.log(newCommentText);
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -30,7 +32,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório')
   }
 
   function deleteComment(commentToDelete) {
@@ -40,6 +47,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+  
+  const isNewCommentEmpty = newCommentText.length === 0;
   
   return (
     <article className={styles.post}>
@@ -74,9 +83,13 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
